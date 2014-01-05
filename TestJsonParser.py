db9cbf594c7a27eac3b448cd592879800ba7d052
Bin LI <richardustc@gmail.com>
2014-01-05 17:21:20 +0800

    add some Json test in TestJsonParser.py

--- TestJsonParser.py
+++ TestJsonParser.py
@@ -4,7 +4,27 @@
 
 # 本文件用于测试JsonParser.py
 
-import JsonParser
+from JsonParser import *
+import sys
+import pdb
+
+testcases = [
+'{}',
+'{"number":1}',
+'{"string":"string"}',
+'{"true": true}',
+'{"false": false}',
+'{"null": null}',
+'{"array": []}',
+'{"array":[1, "s" , true, false, null]}',
+'{"array":[[1, 2, 3], [true, false], [null] ]}',
+'{"array":[{"number":1}, {"string":"string"} ]}',
+'{"object": {}}',
+'{"object": {"array": [1, true, {"object": "object"}]}}'
+]
 
 print "Start testing..."
+p = JsonParser()
+for s in testcases:
+	p.load(s)
 print "End testing..."
